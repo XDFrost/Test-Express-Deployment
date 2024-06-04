ea38aa7664020bd46554f70e49f3b49798a57bbb
Pratham Gupta <pratham0gupta@gmail.com>
2024-06-04 11:57:36 +0530

    chore: Add GitHub API endpoint to retrieve user data

--- index.js
+++ index.js
@@ -5,6 +5,42 @@ const app = express()
 // const port = 3000
 const port = process.env.PORT
 
+const github_data = {
+    "login": "XDFrost",
+    "id": 67687376,
+    "node_id": "MDQ6VXNlcjY3Njg3Mzc2",
+    "avatar_url": "https://avatars.githubusercontent.com/u/67687376?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/XDFrost",
+    "html_url": "https://github.com/XDFrost",
+    "followers_url": "https://api.github.com/users/XDFrost/followers",
+    "following_url": "https://api.github.com/users/XDFrost/following{/other_user}",
+    "gists_url": "https://api.github.com/users/XDFrost/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/XDFrost/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/XDFrost/subscriptions",
+    "organizations_url": "https://api.github.com/users/XDFrost/orgs",
+    "repos_url": "https://api.github.com/users/XDFrost/repos",
+    "events_url": "https://api.github.com/users/XDFrost/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/XDFrost/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Pratham Gupta",
+    "company": "Chitkara",
+    "blog": "Discord UID: 962605552578478130",
+    "location": "India",
+    "email": null,
+    "hireable": true,
+    "bio": "CS Student || Developer || Specialization in AI || Blockchain, ML and DL Enthusiast || \r\n\r\nRepo Maintainer at @Open-Source-Chandigarh ",
+    "twitter_username": null,
+    "public_repos": 31,
+    "public_gists": 1,
+    "followers": 7,
+    "following": 13,
+    "created_at": "2020-07-01T10:06:37Z",
+    "updated_at": "2024-05-23T12:29:26Z"
+  }
+
+
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
@@ -19,6 +55,10 @@ app.get('/login', (req, res) => {
 
 app.get('/youtube' , (req, res) => {
     res.send("<h2> This is YT url </h2>")
+})
+
+app.get('/github', (req, res) => {
+    res.json(github_data)
 })
 
 // app.listen(port, () => {
